51c208d39879b1242024bc3c01c7327988ee6730
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-05 21:54:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <p>Nome dos alunos na equipe: Igor e Johan</p>
   <a href="https://igorbecker2.herokuapp.com/fluxograma.pdf" download="fluxograma.pdf">PDF para ser baixado conforme solicitado</a>
   <p></p>
-  <form action="paginaB.php" method="post">
+  <form action="calculo.php" method="post">
    Valor 1:<input type="text" name="valor1" size="2" maxlength="2">
    Valor 2:<input type="text" name="valor2" size="2" maxlength="2">
    <button type="button">Enviar valores</button>
