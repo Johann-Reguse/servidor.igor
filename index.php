b76c5a570d18f4b1fa88a2a9c4a07f2750a1e030
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-05 21:37:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,6 @@
   <br>
   Valor 1:<input type="text" name="valor1" size="2" maxlength="2">
   Valor 2:<input type="text" name="valor2" size="2" maxlength="2">
-  
+  <button>Enviar valores</button>
  </body>
 </html>
