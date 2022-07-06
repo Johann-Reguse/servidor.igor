d92a16720baee8a3489cff9cf6b6160b05c63ac6
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-05 21:40:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,17 @@
   <p>SERVIDOR 1</p>
   <p>Nome dos alunos na equipe: Igor e Johan</p>
   <a href="https://igorbecker2.herokuapp.com/fluxograma.pdf" download="fluxograma.pdf">PDF para ser baixado conforme solicitado</a>
-  <br>
+  <p></p>
   Valor 1:<input type="text" name="valor1" size="2" maxlength="2">
   Valor 2:<input type="text" name="valor2" size="2" maxlength="2">
-  <button>Enviar valores</button>
+
+  <button type="button" onclick="Evento()">Enviar valores</button>
+
+   <script>
+    function Evento(){
+     alert('evento funcionando')
+    }
+   </script>
+  
  </body>
 </html>
