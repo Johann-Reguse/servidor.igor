349476aeace3e82ab97194a279b64ad4477bb805
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-05 21:35:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,7 @@
   <p>SERVIDOR 1</p>
   <p>Nome dos alunos na equipe: Igor e Johan</p>
   <a href="https://igorbecker2.herokuapp.com/fluxograma.pdf" download="fluxograma.pdf">PDF para ser baixado conforme solicitado</a>
+  Valor 1:<input type="text" name="valor1" size="2" maxlength="2">
+  Valor 2:<input type="text" name="valor2" size="2" maxlength="2">
  </body>
 </html>
