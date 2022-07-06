af34576162819186bdfe6d45dbc7a47f8708f1f4
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-05 22:24:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
   <a href="https://igorbecker2.herokuapp.com/fluxograma.pdf" download="fluxograma.pdf">PDF para ser baixado conforme solicitado</a>
   <p></p>
   <form action="calculo.php" method="post">
-   Valor 1<input type="text" name="valor1" value="opa" />
-   Valor 2<input type="text" name="valor2" value="opa2" />                                                              
+   Valor 1<input type="text" name="valor1" value="" />
+   Valor 2<input type="text" name="valor2" value="" />                                                              
    <input type="submit" value="Enviar valores">
   </form>
   
