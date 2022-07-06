d888223bcd5ad3ab9c99b54b95345056cf0079b3
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-05 22:35:50 -0300

    Update calculo.php

--- calculo.php
+++ calculo.php
@@ -1,6 +1,6 @@
 <?php
 $SOMA = $_POST['valor1'] + $_POST['valor2'];
 
-$result = pow(number $SOMA, number 2);
-echo "Quadrado da soma: $SOMA";
+$result = pow($SOMA, 2);
+echo "Quadrado da soma: $result";
 ?>
