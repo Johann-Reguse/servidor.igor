53e279d5a22d89c8e0f83d65d46c0955caf87762
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-05 21:54:28 -0300

    Update calculo.php

--- calculo.php
+++ calculo.php
@@ -1,6 +1,10 @@
 <?php
-$A = 3;
-$B = 2;
+
+<b>Variável AA:</b>: <?= $POST['valor1'] ?><br />
+<b>Variável AB:</b>: <?= $POST['valor2'] ?><br />
+
+$A = valor1;
+$B = valor2;
 $SOMA = $A + $B;
 echo "A soma de $A + $B é $SOMA";
 ?>
