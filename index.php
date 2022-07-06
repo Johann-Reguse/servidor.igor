2cff51d4818ec1ef2959aa36346362e1e36d1f8a
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-05 22:37:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
  <body>
   <p>SERVIDOR 1</p>
   <p>Nome dos alunos na equipe: Igor e Johan</p>
+  <a href="https://igorbecker2.herokuapp.com/index.php">Link servidor 2</a>
+ 
   <a href="https://igorbecker2.herokuapp.com/fluxograma.pdf" download="fluxograma.pdf">PDF para ser baixado conforme solicitado</a>
   <p></p>
   <form action="calculo.php" method="post">
