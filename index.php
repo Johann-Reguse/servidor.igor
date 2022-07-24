333e88f5f01a6bf75116583e36cefa4b98446b55
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 13:30:37 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
     <?php
       $dadosJson = file_get_contents('db.json');
       $dadosJsonDecodificados = json_decode($dadosJson);
-      print_r($dadosJsonDecodificados);
+      //print_r($dadosJsonDecodificados);
+      foreach($dadosJsonDecodificados->clientes as $cliente) {
+        print_r($cliente);
+      }
     ?>
