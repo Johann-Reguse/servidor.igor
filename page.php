0d05c55dc462cf6d685229593369dadb357af966
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 14:50:58 -0300

    Update page.php

--- page.php
+++ page.php
@@ -1,14 +1,9 @@
 <?php
 	$path = explode('/', $_GET['path']);
 	$dadosJson = file_get_contents('db.json');
-	  
 	$json = json_decode($dadosJson, true);
-	  
-	$method = $_SERVER['REQUEST_METHOD'];
-  	
-	header('Content-type: application/json');
-	$body = file_get_contents('php://input');
 	
-	echo json_encode($json[$path[0]]);
-
+	if($POST) {	
+		echo json_encode($json[$path[0]]);
+	}
 ?>
