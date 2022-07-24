ac66b5f0df7e607efdd18b43892adffbab3e623d
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 15:15:08 -0300

    Update page.php

--- page.php
+++ page.php
@@ -1,8 +1,6 @@
 <?php
 	$dadosJson = file_get_contents('db.json');
 	$dadosJsonDecodificados = json_decode($dadosJson);
-	print_r($dadosJsonDecodificados);
-	
 	if($_POST['via'] == "0" ) {
 		print_r($dadosJsonDecodificados);
 	} 
