6260a2b9d177dd195ee420d182ac3809c987c824
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 15:30:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,23 @@
             <input type="text" name="via" placeholder="">
             <br>
             <input type="text" name="nome" placeholder="Nome autor">
+            <br>
             <input type="text" name="sobrenome" placeholder="Sobrenome autor">
+            <br>
             <input type="text" name="titulo" placeholder="Título do livro">
+            <br>
             <input type="text" name="editora" placeholder="Editora">
+            <br>
             <input type="text" name="cidade" placeholder="Cidade da publicação">
+            <br>
             <input type="text" name="ano" placeholder="Ano da publicação">
+            <br>
             <input type="text" name="paginas" placeholder="Quantidade de páginas">
+            <br>
             <input type="text" name="isbn" placeholder="ISBN">
+            <br>
             <input type="text" name="assuntos" placeholder="Assuntos abordados no livro">
+            <br>
             <input type="submit" value="batata">
         </form>
     </body>    
