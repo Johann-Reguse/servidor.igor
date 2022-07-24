94b92689a1d71393a879a3102240906943418e40
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 15:13:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
     <body>
         <p>ola mundo</p>
         <a href="https://igorbecker.herokuapp.com/page.php">Verificar dados do Json</a>
-        <form action="page.php" method="GET">
-            <input type="submit" value="obter dados via método GET">
+        <form action="page.php" method="POST">
+            <input type="text" name="via" placeholder="0 para pegar valores">
+            <input type="submit" value="batata">
         </form>
     </body>    
 </html>
