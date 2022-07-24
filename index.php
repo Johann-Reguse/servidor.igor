46b03dd75096ea4a2da666f6a7ba90ab7f285535
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 14:20:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-    <?php
-
-      echo "ola mundo";
-       
-    ?>
+<html>
+    <head>
+        <title>pagina de requisição GET e POST</title>
+    </head>
+    <body>
+        <p>ola mundo</p>
+        <form action="" method="GET">
+            <input type="text" name="id" placeholder="nome">
+            <input type="submit" value="enviar">
+        </form>
+    </body>    
+</html>
