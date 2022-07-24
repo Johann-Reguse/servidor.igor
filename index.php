8541d03c25e06fbb1f7b7fb02f9fc4ffaa7e70b9
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 14:30:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
         <p>ola mundo</p>
         <a href="https://igorbecker.herokuapp.com/page.php">Verificar dados do Json</a>
         <form action="page.php" method="GET">
-            //<input type="text" name="id" placeholder="id">
-            //<input type="text" name="nome" placeholder="nome">
             <input type="submit" value="enviar">
         </form>
     </body>    
