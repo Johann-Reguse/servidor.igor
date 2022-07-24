760a237cc18491a0aef4017f4f7b70b3a831801a
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 15:27:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,17 @@
         <title>pagina de requisição GET e POST</title>
     </head>
     <body>
-        <p>ola mundo</p>
-        <a href="https://igorbecker.herokuapp.com/page.php">Verificar dados do Json</a>
         <form action="page.php" method="POST">
-            <input type="text" name="via" placeholder="0 para pegar valores">
+            <input type="text" name="via" placeholder="0 visualizar banco de dados / 1 enviar dados">
+            <input type="text" name="nome" placeholder="Nome autor">
+            <input type="text" name="sobrenome" placeholder="Sobrenome autor">
+            <input type="text" name="titulo" placeholder="Título do livro">
+            <input type="text" name="editora" placeholder="Editora">
+            <input type="text" name="cidade" placeholder="Cidade da publicação">
+            <input type="text" name="ano" placeholder="Ano da publicação">
+            <input type="text" name="paginas" placeholder="Quantidade de páginas">
+            <input type="text" name="isbn" placeholder="ISBN">
+            <input type="text" name="assuntos" placeholder="Assuntos abordados no livro">
             <input type="submit" value="batata">
         </form>
     </body>    
