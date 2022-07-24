cb48f5c5630ccd1c002f2eecdcf382a425007bfc
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 12:05:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
     <?php
-      $path = explode('/', $_GET['path']);
-      $contents = file_get_contents('db.json');
-	     $json = json_decode($contents, true);
-	     $method = $_SERVER['REQUEST_METHOD'];
-	     header('Content-type: application/json');
-	     $body = file_get_contens('php://input');
-	     if($method === 'GET'){
-		      if($json[$path[0]) {
-			       echo json_encode($json[$path[0]]);
-	 	     }else {
-			       echo '[]';
-		      }
-	     }
+      $dadosJson = file_get_contents('db.json');
+      print_r($dadosJson);
     ?>
