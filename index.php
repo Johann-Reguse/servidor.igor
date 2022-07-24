588815d82bd993d30790ebd9e0822acf87897653
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 13:24:30 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
     <?php
       $dadosJson = file_get_contents('db.json');
       $dadosJsonDecodificados = json_decode($dadosJson);
+      print_r($dadosJson);
       print_r($dadosJsonDecodificados);
     ?>
