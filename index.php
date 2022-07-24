89d9b541b3955257b25396caaff51c75bccc943b
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 11:48:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
 <html>
- <head>
-  <title>Teste PHP</title>
- </head>
- <body>
-  <p>SERVIDOR 1</p>
-  <p>Nome dos alunos na equipe: Igor e Johan</p>
+  <head>
+    <title>Teste PHP</title>
+  </head>
+  <body>
+    <p>SERVIDOR 1</p>
+    <p>Nome dos alunos na equipe: Igor e Johan</p>
   
-  <?php
-    echo "ola mundoooooooo";
-  ?>
+    <?php
+      echo "ola mundoooooooo";
+      $path = explode('/', $_GET['path']);
+      $contents = file_get_contens('db.json');
+	     $json = json_decode($contens, true);
+	     $method = $_SERVER['REQUEST_METHOD'];
+	     header('Content-type: application/json');
+	     $body = file_get_contens('php://input');
+	     if($method === 'GET'){
+		      if($json[$path[0]) {
+			       echo json_encode($json[$path[0]]);
+	 	     }else {
+			       echo '[]';
+		      }
+	     }
+    ?>
+ 
   
   
   <a href="https://igorbecker2.herokuapp.com/index.php">Link servidor 2</a>
