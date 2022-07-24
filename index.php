b4a0ae895616010e1e5f6dac228cf38fddd3e68f
Johann-Reguse <51337281+Johann-Reguse@users.noreply.github.com>
2022-07-24 16:09:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
+<?php
+   session_start();
+   if (isset ($_POST['via'])){
+            $_SESSION['Livro'][] = jason_encode($_POST);
+            echo <'Livro INserido">
+   }
+?>
+
+
+
 <html>
     <head>
         <title>pagina de requisição GET e POST</title>
     </head>
     <body>
-        <form action="page.php" method="POST">
+        
+        if (isset ($_POST['via'])){
+            $_SESSION['Livro'][] = jason_encode($_POST);
+            
+            echo <'Livro INserido">
+            
+        }
+
+        <!DOCTYPE html>
+        
+        // Campos a serem preenchidos
+        <form method="post">
             <p>0 visualizar banco de dados / 1 enviar dados</p>
             <input type="text" name="via" placeholder="0 ou 1">
             <br>
-            <input type="text" name="nomeAutor" placeholder="Nome autor">
+            <input type="text" name="nome" placeholder="Nome autor">
             <br>
             <input type="text" name="sobrenome" placeholder="Sobrenome autor">
             <br>
@@ -25,7 +46,18 @@
             <br>
             <input type="text" name="assuntos" placeholder="Assuntos abordados no livro">
             <br>
-            <input type="submit" value="batata">
+            <input type="submit" value="Inserir Livro">
         </form>
+        
+        <form method="post">
+            Preencher para mostrar <br>
+            Livro específico:(código) <br>
+            <input type="text" name="LivroEspecifico"><br><br>
+            <input type="submit" name="mostrar" value="Mostrar Livros">
+         <\form>
+         <br>
+         <form method="post">
+            <input type="submit" name="limpar" value="limpar cache">
+            <\form><br>
     </body>    
 </html>
