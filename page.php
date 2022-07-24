30b48cefb868ce25649ddb95eb68400ef6c2e853
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 14:15:33 -0300

    Create page.php

---
+++ page.php
@@ -0,0 +1,21 @@
+    <?php
+
+      $path = explode('/', $_GET['path']);
+      $dadosJson = file_get_contents('db.json');
+	  
+	  $json = json_decode($dadosJson, true);
+	  
+	  $method = $_SERVER['REQUEST_METHOD'];
+	  
+	  header('Content-type: application/json');
+	  $body = file_get_contents('php://input');
+	  
+	  if($method === 'GET'){
+		if($json[$path[0]]) {
+			echo json_encode($json[$path[0]]);
+		}else {
+			echo '[]';
+		}
+	  }
+       
+    ?>
