00d020b560a878770cc833fa4bb9a101df29ec9d
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 15:09:39 -0300

    Update page.php

--- page.php
+++ page.php
@@ -1,9 +1,9 @@
 <?php
 	$dadosJson = file_get_contents('db.json');
 	$dadosJsonDecodificados = json_decode($dadosJson);
-	//print_r($dadosJsonDecodificados);
+	print_r($dadosJsonDecodificados);
 	
-	if($_GET) {
-		print_r($dadosJsonDecodificados);
-	} 
+	//if($_GET) {
+		//print_r($dadosJsonDecodificados);
+	//} 
 ?>
